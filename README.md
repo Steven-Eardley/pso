304cb232b76b6676af2df5c5340a4ac9274853fa
Steven Eardley <steve@eardley.xyz>
2017-05-03 22:26:00 +0100

    added demos for opencl and matplotlib

---
+++ README.md
@@ -0,0 +1,11 @@
+## PSO
+
+### We like the newest pip and wheel to aid installation.
+
+```
+pip install --upgrade pip
+pip install --upgrade wheel
+pip install --upgrade setuptools
+
+pip install -e . --no-cache-dir --ignore-installed
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name = 'pso',
+    version = '0.0.1',
+    packages = find_packages(),
+    install_requires = [
+        "ipython",
+	"numpy",
+	"matplotlib",
+	"mako",
+        "pyopencl",
+    ],
+    url = 'http://eardley.xyz',
+    author = 'Steven Eardley',
+    author_email = 'steve@eardley.xyz',
+    description = 'Particle swarm optimisation with OpenCL',
+    license = 'Copyheart',
+    classifiers = [
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: Copyheart',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
+
